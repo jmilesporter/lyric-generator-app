a626d258c057a970e8d82fa5644221e0f4c1c6d2
Thomas Walsh <Thomas.D.Walsh@kp.org>
2023-01-13 23:24:51 -0500

    first attempt at implementing conditionals for payload string

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 
 	export let data;
 	const apiKey = data.key;
+	const temperature = 2000
 
 	async function onClick() {
 		$lyrics = '';
@@ -20,7 +21,7 @@
 				body: JSON.stringify({
 					model: 'text-davinci-003',
 					prompt: lyricPrompt,
-					temperature: 0.8,
+					temperature: temperature,
 					max_tokens: 4000
 				})
 			});
@@ -43,73 +44,45 @@
 	$: subject = '';
 	$: genre = '';
 	$: artist = '';
-	$: structure = '';
+	$: songTitle = '';
 
-	switch (structure) {
-		case 'verse-verse-verse':
-			structure = 'verse-verse-verse';
-			break;
-		case 'verse-chorus':
-			structure = 'verse-chorus-verse-chorus-verse-chorus';
-			break;
-		case 'verse-bridge':
-			structure = 'verse-verse-bridge-verse-bridge-verse';
-			break;
-		case 'verse-prechorus-chorus':
-			structure = 'verse-prechorus-chorus-verse-prechorus-chorus';
-			break;
-		default:
-			structure = '';
+	$: lyricPrompt = '';
+	if (genre) {
+	$: lyricPrompt = `${genre} lyrics `;
+	}
+	if (subject) {
+	$: lyricPrompt = lyricPrompt.concat(` on the subject of ${subject}`)
+	}
+	if (artist) {
+	$: lyricPrompt = lyricPrompt.concat(` in the style of ${artist}`)
+	}
+	if (songTitle) {
+	$: lyricPrompt = lyricPrompt.concat(` called ${songTitle}`)
 	}
 
-	$: lyricPrompt = `${genre} lyrics  on the subject of ${subject} in the style of ${artist}, ${structure}`;
 </script>
 
 <div class="grid place-items-center bg-slate-500 h-screen ">
 	<input
-		placeholder="s u b j e c t"
+		placeholder="s u b j e c t ( s )"
 		class="absolute top-0 placeholder-white focus:placeholder-transparent text-center bg-slate-700 p-8 w-72 shadow-xl rounded-3xl text-white hover:translate-y-1 hover:scale-110 hover:bg-slate-600 hover: transition ease-in-out duration-500 delay-300  "
 		bind:value={subject}
 	/>
-	<select
-		class="absolute right-0 text-center bg-slate-700  w-72 p-8 shadow-xl rounded-3xl text-gray-200 hover:-translate-x-1 hover:scale-110 hover:bg-slate-600 hover: transition ease-in-out duration-500 delay-300  "
-		bind:value={genre}
-	>
-		<option class="rotate-90" value="" disabled selected>g e n r e</option>
-
-		<option>rap</option>
-		<option>rock</option>
-		<option>pop</option>
-		<option>folk</option>
-		<option>country</option>
-		<option>blues</option>
-		<option>r&b</option>
-		<option>metal</option>
-		<option>classical</option>
-		<option>reggae</option>
-		<option>electronic</option>
-		<option>indie</option>
-		<option>punk</option>
-		<option>disco</option>
-		<option>house</option>
-		<option>techno</option>
-	</select>
 	<input
-		placeholder="a r t i s t"
+	placeholder="g e n r e ( s )"
+	class="absolute top-0 placeholder-white focus:placeholder-transparent text-center bg-slate-700 p-8 w-72 shadow-xl rounded-3xl text-white hover:translate-y-1 hover:scale-110 hover:bg-slate-600 hover: transition ease-in-out duration-500 delay-300  "
+	bind:value={genre}
+	/>
+	<input
+		placeholder="a r t i s t ( s )"
 		class="absolute bottom-0 placeholder-white focus:placeholder-transparent text-center bg-slate-700  p-8 w-72 shadow-xl rounded-3xl text-gray-200 hover:-translate-y-1 hover:scale-110 hover:bg-slate-600 hover: transition ease-in-out duration-500 delay-300  "
 		bind:value={artist}
 	/>
-	<select
-		class="absolute absolute left-0 text-center bg-slate-700  w-72 p-8 shadow-xl rounded-3xl text-gray-200 hover:translate-x-1 hover:scale-110 hover:bg-slate-600 hover: transition ease-in-out duration-500 delay-300  "
-		bind:value={structure}
-	>
-		<option value="" disabled selected>s t r u c t u r e</option>
-		<option>verse-verse-verse</option>
-		<option>verse-chorus</option>
-		<option>verse-bridge</option>
-		<option>verse-chorus-bridge</option>
-		<option>verse-prechorus-chorus</option>
-	</select>
+	<input
+	placeholder="s u b j e c t ( s )"
+	class="absolute top-0 placeholder-white focus:placeholder-transparent text-center bg-slate-700 p-8 w-72 shadow-xl rounded-3xl text-white hover:translate-y-1 hover:scale-110 hover:bg-slate-600 hover: transition ease-in-out duration-500 delay-300  "
+	bind:value={songTitle}
+	/>
 
 	<button
 		id="generate"
