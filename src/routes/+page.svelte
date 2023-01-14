17827be5354f52661b54d3c87fac7f6a5249a23c
Thomas Walsh <Thomas.D.Walsh@kp.org>
2023-01-14 02:40:43 -0500

    added predictability range input and fixed small bugs with prompt text implementation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,33 @@
 
 	export let data;
 	const apiKey = data.key;
-	const temperature = 2000
+	let rangeValue = 70
+
+
+	function formatPayload() {
+		if (genre) {
+			lyricPrompt = `${genre} `;
+		}
+			lyricPrompt = `lyrics`
+		if (subject) {
+			lyricPrompt = lyricPrompt.concat(` on the subject of ${subject}`)
+		}
+		if (artist) {
+			lyricPrompt = lyricPrompt.concat(` in the style of ${artist}`)
+		}
+		if (songTitle) {
+			lyricPrompt = lyricPrompt.concat(` of a song named ${songTitle}`)
+		}
+		if (lyricPrompt === 'lyrics') {
+			lyricPrompt = 'lyrics on any subject in any style';
+		}
+	}
+
 
 	async function onClick() {
 		$lyrics = '';
 		isOverlayOpen.set(true);
+		formatPayload();
 		try {
 			$loading = true;
 			const response = await fetch('https://api.openai.com/v1/completions', {
@@ -21,7 +43,7 @@
 				body: JSON.stringify({
 					model: 'text-davinci-003',
 					prompt: lyricPrompt,
-					temperature: temperature,
+					temperature: (150 - rangeValue)/100,
 					max_tokens: 4000
 				})
 			});
@@ -47,18 +69,6 @@
 	$: songTitle = '';
 
 	$: lyricPrompt = '';
-	if (genre) {
-	lyricPrompt = `${genre} lyrics `;
-	}
-	if (subject) {
-	lyricPrompt = lyricPrompt.concat(` on the subject of ${subject}`)
-	}
-	if (artist) {
-	lyricPrompt = lyricPrompt.concat(` in the style of ${artist}`)
-	}
-	if (songTitle) {
-	lyricPrompt = lyricPrompt.concat(` called ${songTitle}`)
-	}
 
 </script>
 
@@ -70,7 +80,7 @@
 	/>
 	<input
 	placeholder="g e n r e ( s )"
-	class="absolute top-0 placeholder-white focus:placeholder-transparent text-center bg-slate-700 p-8 w-72 shadow-xl rounded-3xl text-white hover:translate-y-1 hover:scale-110 hover:bg-slate-600 hover: transition ease-in-out duration-500 delay-300  "
+	class="absolute right-0 placeholder-white focus:placeholder-transparent text-center bg-slate-700 p-8 w-72 shadow-xl rounded-3xl text-white hover:translate-y-1 hover:scale-110 hover:bg-slate-600 hover: transition ease-in-out duration-500 delay-300  "
 	bind:value={genre}
 	/>
 	<input
@@ -79,10 +89,17 @@
 		bind:value={artist}
 	/>
 	<input
-	placeholder="s u b j e c t ( s )"
-	class="absolute top-0 placeholder-white focus:placeholder-transparent text-center bg-slate-700 p-8 w-72 shadow-xl rounded-3xl text-white hover:translate-y-1 hover:scale-110 hover:bg-slate-600 hover: transition ease-in-out duration-500 delay-300  "
+	placeholder="s o n g   t i t l e"
+	class="absolute left-0 placeholder-white focus:placeholder-transparent text-center bg-slate-700 p-8 w-72 shadow-xl rounded-3xl text-white hover:translate-y-1 hover:scale-110 hover:bg-slate-600 hover: transition ease-in-out duration-500 delay-300  "
 	bind:value={songTitle}
 	/>
+	<!-- <div class="absolute bottom-60 placeholder-white focus:placeholder-transparent text-center bg-slate-700 p-8 w-72 shadow-xl rounded-3xl text-white "> -->
+	<div class="absolute bottom-60 placeholder-white focus:placeholder-transparent text-center bg-slate-700 p-2 w-60 shadow-xl rounded-3xl text-white ">
+			<div>p r e d i c t a b i l i t y</div>
+			<label>
+				<input class="hover:translate-x-1 hover:scale-110 hover:bg-slate-600 hover: transition ease-in-out duration-500 delay-300 accent-[#fb8200]" type=range bind:value={rangeValue} min=0 max=150>
+			</label>
+	</div>
 
 	<button
 		id="generate"
@@ -92,7 +109,6 @@
 		}}>g e n e r a t e &nbsp; l y r i c s</button
 	>
 </div>
-
 <style>
 	select {
 		appearance: none;
