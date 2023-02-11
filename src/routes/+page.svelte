56433d609f58fb68e777b4ffedc6fcebdb29e618
Jeffrey Porter <jmilesporter@gmail.com>
2023-02-11 00:43:14 -0500

    change layout, add help button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,20 +24,26 @@
 		}
 	}
 
-	let hasBeenClicked = false;
+	function helpModal() {
+		lyrics.set(
+			`This is a prompt engineering interface for OpenAI's GPT-3 API. It's a text generator that can write lyrics in the style of any artist. You can also specify a subject to write about. The more specific you are, the better the results.`
+		);
+		isOverlayOpen.set(true);
+	}
+	let clicked = false;
 
 	function preventMultiClick() {
-		if (hasBeenClicked) return;
+		if (clicked) return;
 
-		hasBeenClicked = true;
+		clicked = true;
 		setTimeout(() => {
-			hasBeenClicked = false;
+			clicked = false;
 		}, 3000);
 
 		// console.log('click');
 	}
 
-	async function onClick() {
+	async function generateLyrics() {
 		preventMultiClick();
 
 		$lyrics = '';
@@ -59,6 +65,7 @@
 				})
 			});
 			const data = await response.json();
+
 			$loading = false;
 
 			$lyrics = data.choices[0].text;
@@ -81,7 +88,13 @@
 	$: lyricPrompt = '';
 </script>
 
-<div class="grid place-items-center bg-slate-500 h-screen ">
+<div class="grid place-items-center bg-slate-900 h-screen ">
+	<button
+		class="absolute full:top-4 full:right-4 xs:bottom-48 placeholder-white focus:placeholder-transparent text-center bg-slate-700 w-12 h-12 shadow-xl rounded-full text-gray-200 hover:-translate-x-1 hover:scale-110 hover:bg-slate-600 hover: transition ease-in-out duration-500 delay-400  "
+		on:click={() => {
+			helpModal();
+		}}>?</button
+	>
 	<input
 		placeholder="s u b j e c t ( s )"
 		class="absolute xs:top-0 full:top-0 full:top-none placeholder-white focus:placeholder-transparent text-center bg-slate-700 p-8 w-72 shadow-xl rounded-3xl text-white hover:translate-y-1 hover:scale-110 hover:bg-slate-600 hover: transition ease-in-out duration-500 delay-300  "
@@ -104,12 +117,12 @@
 	/> -->
 	<!-- <div class="absolute bottom-60 placeholder-white focus:placeholder-transparent text-center bg-slate-700 p-8 w-72 shadow-xl rounded-3xl text-white "> -->
 	<div
-		class="absolute xs:bottom-12 full:bottom-60 placeholder-white focus:placeholder-transparent text-center bg-slate-700 p-2 w-60 shadow-xl rounded-3xl text-white "
+		class="absolute xs:bottom-24 full:bottom-60 placeholder-white focus:placeholder-transparent text-center bg-slate-700 p-2 w-60 shadow-xl rounded-3xl text-white "
 	>
 		<div>p r e d i c t a b i l i t y</div>
 		<label>
 			<input
-				class="hover:translate-x-1 hover:scale-110 hover:bg-slate-600 hover: transition ease-in-out duration-500 delay-300 accent-[#fb8200]"
+				class="hover:translate-x-1 hover:scale-110 hover:bg-slate-600 hover: transition ease-in-out duration-500 delay-300 hover:accent-[#ffa64c] accent-[#fb8200]"
 				type="range"
 				bind:value={rangeValue}
 				min="0"
@@ -120,10 +133,15 @@
 
 	<button
 		id="generate"
-		class=" absolute bg-slate-900 xs:top-96  p-8 w-72 shadow-xl rounded-3xl text-gray-200 hover:-translate-y-1 hover:scale-110 hover:bg-slate-800 hover: transition ease-in-out duration-500 delay-300  "
+		class=" absolute bg-[#fb8200] xs:top-96  p-8 w-72 shadow-xl rounded-3xl text-slate-900 hover:-translate-y-1 hover:scale-110 hover:bg-[#ffa64c] hover: transition ease-in-out duration-500 delay-300  "
 		on:click={() => {
-			onClick();
+			generateLyrics();
 		}}>g e n e r a t e &nbsp; l y r i c s</button
+	>
+	<a
+		href="https://docs.google.com/forms/d/e/1FAIpQLSdMJ0woyXr-PSIVSFEBsI9Vk5ofXol2F4JXR6mbUL2VHYBvkQ/viewform"
+		class="absolute xs:bottom-12 full:bottom-12 placeholder-white focus:placeholder-transparent text-center rounded-3xl text-white "
+		>s u b m i t &nbsp; f e e d b a c k</a
 	>
 </div>
 
